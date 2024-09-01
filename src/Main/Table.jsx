fd7e4050f7aa4d7f5d431231c2e8a2d0be8aaf05
akif <su92-bssem-f22-118@superior.edu.pk>
2024-09-01 10:14:49 +0500

    update whole table style
    update

--- src/Main/Table.jsx
+++ src/Main/Table.jsx
@@ -2,25 +2,36 @@ import React from 'react'
 
 const Table = () => {
   return (
-   <>
-      <div className='flex justify-center item-center '>
-        <table  className=' '>
-            <tr className='m-5'>
-                <th>Mark Attendence</th>
-                <th> Mark Leave</th>
-            <th>view</th>
-        </tr>
-        <tr>
-            <td><button className='bg-red-600 w-full border rounded-md' >Absent</button></td>
-            <td><button className='bg-green-500 w-full border rounded-md'>Leave</button></td>
-            <td><button className='bg-slate-300 w-full border rounded-md'>View</button></td>
-            
-        </tr>
-        
-    </table>
+    <div className="flex justify-center items-center">
+      <table className="border-2 border-black">
+        <thead>
+          <tr>
+            <th>Mark Attendance</th>
+            <th>Mark Leave</th>
+            <th>View</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <button className="bg-red-600 w-full border rounded-md">
+                Absent
+              </button>
+            </td>
+            <td>
+              <button className="bg-green-500 w-full border rounded-md">
+                Leave
+              </button>
+            </td>
+            <td>
+              <button className="bg-slate-300 w-full border rounded-md">
+                View
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-     
-   </>
   )
 }
 
