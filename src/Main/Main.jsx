ea122551e29bddc951b3ef15e3a09301709f6f42
akif <su92-bssem-f22-118@superior.edu.pk>
2024-09-01 09:52:30 +0500

    added tale
    main page content

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 import Navbar from './Navbar'
 const Main = () => {
   return (
- <>
- <div>
-  <nav>
-  <Navbar/>
-  </nav>
- </div>
- </>
+    <>
+      <div>
+        <nav>
+          <Navbar />
+        </nav>
+      </div>
+      <main>
+        <div >
+        <table>
+        <tr>
+            <th>Mark Attendence</th>
+            <th>Mark Leave</th>
+            <th>view</th>
+        </tr>
+        <tr>
+            <td><button>Absent</button></td>
+            <td><button>Leave</button></td>
+            <td><button>View</button></td>
+            
+        </tr>
+        
+    </table>
+        </div>
+      </main>
+    </>
   )
 }
 
