79340de85a277244ba74be80f6f5da27a473b31f
akif <su92-bssem-f22-118@superior.edu.pk>
2024-09-01 10:10:09 +0500

    ss
    sss

--- src/Main/Table.jsx
+++ src/Main/Table.jsx
@@ -4,14 +4,14 @@ const Table = () => {
   return (
    <>
       <div className='flex justify-center item-center '>
-        <table  className=' border-2 border-black '>
-            <tr className=''>
+        <table  className=' '>
+            <tr className='m-5'>
                 <th>Mark Attendence</th>
                 <th> Mark Leave</th>
             <th>view</th>
         </tr>
         <tr>
-            <td><button className='bg-red-600 w-100 border border-rounded-4' >Absent</button></td>
+            <td><button className='bg-red-600 w-96 border border-rounded-4' >Absent</button></td>
             <td><button className='bg-green-500 w-100'>Leave</button></td>
             <td><button className='bg-slate-300 w-100'>View</button></td>
             
